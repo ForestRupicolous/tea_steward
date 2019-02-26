c83aa094d04d36bfcd825b28e970af91d6b867d5
Martin Korinek <work@korinek.cc>
2019-02-26 16:24:26 +0100

    Add neopixel lib to use left over pin to debug

--- code/Tea Steaward/src/main.cpp
+++ code/Tea Steaward/src/main.cpp
@@ -1,5 +1,10 @@
-#include <Arduino.h>
+
 #include <CheapStepper.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
 
 CheapStepper stepper (5,6,7,3); 
 // here we declare our stepper using default pins:
@@ -9,13 +14,33 @@ CheapStepper stepper (5,6,7,3);
 // 10 <--> IN3
 // 11 <--> IN4
 
-
 boolean moveClockwise = true;
 
+//Neopixel ++++++++++++++++++++++++++
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            5
 
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      16
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+//End Neopixel defines +++++++++++++++++++++++++
 void setup() {
   
-  // let's just set up a serial connection and test print to the console
+  pixels.begin(); // This initializes the NeoPixel library.
+
+  //setup test, move this to code after inital neopixel test run
+  for(int i=0;i<NUMPIXELS;i++){
+
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+  }
 }
 
 void loop() {
